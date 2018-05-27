2da51576524d829dd57df019b3b1732f6660b845
bigman9 <yangjunjing2010@163.com>
2018-05-27 12:22:18 +0800

    Create renew.sh

---
+++ shell/renew.sh
@@ -0,0 +1,10 @@
+#!bin/bash
+export JAVA_HOME=/home/hadoop/jdk1.8.0_77
+export SPARK_HOME=/home/hadoop/spark-1.6.0-bin-hadoop2.6
+export CLUSTER_HOME=/home/hadoop/spark-jars/cluster/scripts
+export PATH=$JAVA_HOME/bin:$JAVA_HOME/jre/bin:$SPARK_HOME/bin:$SPARK_HOME/sbin:$PATH
+DAILY_LOG="/home/hadoop/spark-jars/cluster/log/renew-`date +%Y%m%d`.log"
+
+spark-submit --class com.yty.spark.analysis.java.kmeanschineseForKeyword.SparkNewClusterDBChinese  --master spark://server911:7077 --jars /home/hadoop/spark-jars/cluster/streaming/analysis.scala.jar --num-executors 2 --executor-memory 6G --executor-cores 3 --driver-memory 4G --total-executor-cores 6 --conf spark.default.parallelism=12 /home/hadoop/spark-jars/cluster/init/analysis.java.jar 2 100 1 800 800 >>$DAILY_LOG 2>&1 &
+
+sudo /etc/drop_caches
